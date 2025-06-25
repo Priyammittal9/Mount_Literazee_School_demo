bb392f6157e765b90bbe189dcb0b5a09164f5423
devHksingh <hksingh.dev@gmail.com>
2025-06-25 22:55:26 +0530

    add api for contact form

--- src/http/api.ts
+++ src/http/api.ts
@@ -10,6 +10,16 @@ export interface FormsPopProps {
     class: string;
 
 }
+ interface FormProps {
+  parentName: string;
+//   childName: string;
+  email?: string;
+  phone: string;
+  age: string;
+//   previousSchoolName?: string;
+  class: string;
+  message: string;
+}
 const api = axios.create({
     baseURL: import.meta.env.VITE_BASE_URL,
     headers: {
@@ -21,7 +31,12 @@ const popForm = async (data: FormsPopProps) => {
     const res = await api.post('', data)
     return res
 }
+const submitForm = async (data: FormProps) => {
+    const res = await api.post('', data)
+    return res
+}
 
 export {
-    popForm
+    popForm,
+    submitForm
 }
